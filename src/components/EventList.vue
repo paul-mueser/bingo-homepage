8a01f2af18aae82aea48d3fb37d428bbe245a3f7
Paul Müser <kontakt@paulmueser.de>
2025-01-13 21:17:20 +0100

    play with first bingo fetching

--- src/components/EventList.vue
+++ src/components/EventList.vue
@@ -34,6 +34,7 @@
       async fetchEvents() {
         try {
           const data = await fetchBingoEvents();
+          console.log(data);
           for (let i = 0; i < data.length; i++) {
             this.events.push(data[i]);
           }
