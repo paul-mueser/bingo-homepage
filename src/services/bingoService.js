243e46e9e1f26a73b9d454ae7a51c35fc2805b3d
Paul Müser <kontakt@paulmueser.de>
2025-06-04 17:01:49 +0200

    update bingoService

--- src/services/bingoService.js
+++ src/services/bingoService.js
@@ -3,12 +3,12 @@ import axios from 'axios';
 const API_URL = '{{API_URL}}';
 
 export const fetchBingoEvents = async () => {
-    const result = await axios.post(`${API_URL}/api/fetch-bingo-events`);
+    const result = await axios.get(`${API_URL}/api/fetch-bingo-events`, { withCredentials: true });
     return result.data;
 };
 
 export const fetchBingoBoard = async (boardname) => {
-    const result = await axios.post(`${API_URL}/api/fetch-bingo-board`, { boardname });
+    const result = await axios.get(`${API_URL}/api/fetch-bingo-board`, { boardname }, { withCredentials: true });
     return result.data;
 }
 
