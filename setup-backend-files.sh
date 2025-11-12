d22694fe04c94c6e8a325797a903e7d94109c7fd
Paul Müser <kontakt@paulmueser.de>
2025-11-12 20:44:41 +0100

    fix backend

--- setup-backend-files.sh
+++ setup-backend-files.sh
@@ -18,6 +18,12 @@ sed -i "s|{{SITE_URL}}|${SITE_URL}|g" api/refresh-token.php
 
 sed -i "s|{{JWT_SECRET}}|${JWT_SECRET}|g" api/verify-token.php
 
+sed -i "s|{{DB_HOST}}|${DB_HOST}|g" api/fetch-users.php
+sed -i "s|{{DB_USER}}|${DB_USER}|g" api/fetch-users.php
+sed -i "s|{{DB_PASS}}|${DB_PASSWORD}|g" api/fetch-users.php
+sed -i "s|{{DB_NAME}}|${DB_NAME}|g" api/fetch-users.php
+sed -i "s|{{JWT_SECRET}}|${JWT_SECRET}|g" api/fetch-users.php
+
 sed -i "s|{{DB_HOST}}|${DB_HOST}|g" api/fetch-bingo-board.php
 sed -i "s|{{DB_USER}}|${DB_USER}|g" api/fetch-bingo-board.php
 sed -i "s|{{DB_PASS}}|${DB_PASSWORD}|g" api/fetch-bingo-board.php
