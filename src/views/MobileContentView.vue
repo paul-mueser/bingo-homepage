b83a7b0c657f85956139c2f4eecc1af2591e74e9
Paul Müser <kontakt@paulmueser.de>
2026-07-11 20:30:35 +0200

    add buttons to switch bingo boards on mobile (closes #208)

--- src/views/MobileContentView.vue
+++ src/views/MobileContentView.vue
@@ -1,6 +1,11 @@
 <template>
   <v-container class="contentWrap">
     <h1>Mobile Content | Game ID: {{ gameId }}</h1>
+    <v-row v-if="value==='showBingoField'" class="justify-center mb-4">
+      <v-col v-for="user in participants" :key="user.id" cols="auto">
+        <v-btn @click="userid = user.id" class="bg-primary" :disabled="userid === user.id">{{ user.username }}</v-btn>
+      </v-col>
+    </v-row>
     <BingoBoard v-if="value==='showBingoField'" :gameId="gameId" :userid="userid"/>
     <EventList v-if="value==='showEvents'" :gameId="gameId"/>
     <Leaderboard v-if="value==='showLeaderboard'" :gameId="gameId"/>
@@ -34,21 +39,36 @@ const value = ref('showBingoField');
   import EventList from '@/components/EventList.vue';
   import Leaderboard from '@/components/Leaderboard.vue';
   import { useUserStore } from '@/stores/user';
+  import { fetchParticipants } from '@/services/bingoService.js';
 
   export default {
     name: 'MobileContent',
+    props: {
+      gameId: String
+    },
     components: {
       BingoBoard,
       EventList,
       Leaderboard
     },
-    props: {
-      gameId: String
-    },
     data() {
       return {
-        userid: useUserStore().id
+        userid: useUserStore().id,
+        participants: []
       }
+    },
+    methods: {
+      async prepareParticipants() {
+        try {
+          const result = await fetchParticipants(this.gameId);
+          this.participants = result.data;
+        } catch (err) {
+          this.participants = [];
+        }
+      }
+    },
+    mounted() {
+      this.prepareParticipants();
     }
   }
 </script>
