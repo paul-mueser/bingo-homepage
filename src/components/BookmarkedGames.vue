6e5129d1879627baa22683d243000be979b03d23
Paul Müser <kontakt@paulmueser.de>
2026-07-11 21:48:34 +0200

    add bookmarked games list on start page (closes #215)

---
+++ src/components/BookmarkedGames.vue
@@ -0,0 +1,52 @@
+<template>
+    <v-container class="contentWrap" v-if="games.length > 0" >
+        <v-list class="game">
+            <v-list-item v-for="game in games" :key="game.gameid">
+                <v-row align="center">
+                    <v-col cols="9/10">
+                        <v-card link :to="`/game/${game.gameid}`" height="50px" style="padding-top: 13px">
+                            {{ game.name }}
+                        </v-card>
+                    </v-col>
+                    <v-col cols="1/10">
+                        <v-btn icon="fa-solid fa-bookmark" @click="removeBookmark(game.gameid)"></v-btn>
+                    </v-col>
+                </v-row>
+            </v-list-item>
+        </v-list>
+    </v-container>
+</template>
+
+<script>
+    import { fetchBingoGames, fetchBookmarkedGames, updateBookmark } from '@/services/bingoService';
+
+    export default {
+        name: 'GamesView',
+        data() {
+            return {
+                games: []
+            }
+        },
+        methods: {
+            async prepareBingoGames() {
+                try {
+                    let result = await fetchBingoGames();
+                    const bingoGames = result.data;
+
+                    result = await fetchBookmarkedGames();
+                    const bookmarkedGames = result.data;
+                    this.games = bingoGames.filter(game => bookmarkedGames.some(bookmarkedGame => bookmarkedGame.gameid === game.gameid));
+                } catch (e) {
+                }
+            },
+
+            async removeBookmark(gameId) {
+                await updateBookmark(gameId, false);
+                this.games = this.games.filter(game => game.gameid !== gameId);
+            }
+        },
+        mounted() {
+            this.prepareBingoGames();
+        }
+    }
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,19 @@
 <template>
   <v-container class="contentWrap">
-    <h1>Willkommen, {{ username }}!</h1>
+    <h1>Willkommen {{ username }}, hier findest du deine Lesezeichen!</h1>
+    <BookmarkedGames/>
   </v-container>
 </template>
 
 <script>
+import BookmarkedGames from '@/components/BookmarkedGames.vue';
 import { useUserStore } from '@/stores/user';
 
 export default {
   name: 'HomeView',
+  components: {
+    BookmarkedGames
+  },
   data() {
     return {
       username: useUserStore().username
